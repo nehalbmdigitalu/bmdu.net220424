f37a41e71bb6976fd46ebd9d722ec5ed5a2c9899
nehalbmdigitalu <162085462+nehalbmdigitalu@users.noreply.github.com>
2024-04-22 19:21:45 +0530

    Add files via upload

---
+++ BMDU PROFILE/script.js
@@ -0,0 +1,137 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("scroll-up").style.display = "block";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+    document.getElementById("scroll-up").style.display = "none";
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//SCROLL ANIMATE
+var scroll =
+  window.requestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
+function loop() {
+  Array.prototype.forEach.call(elementsToShow, function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add("is-visible");
+    } else {
+      element.classList.remove("is-visible");
+    }
+  });
+
+  scroll(loop);
+}
+loop();
+
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+var myVar;
+function myLoader() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+// Button Animation
+
+let link = document.querySelector(".link");
+let pink = document.querySelector(".color");
+
+let hoverTL = gsap.timeline();
+hoverTL.pause();
+
+// from, to, fromTo Tweens
+hoverTL.to(pink, {
+  width: "calc(100% + 1.3em)",
+  ease: "Elastic.easeOut(0.25)",
+  duration: 0.4,
+});
+hoverTL.to(pink, {
+  width: "2em",
+  left: "calc(100% - 1.45em)",
+  ease: "Elastic.easeOut(0.4)",
+  duration: 0.6,
+});
+
+link.addEventListener("mouseenter", () => {
+  hoverTL.play();
+});
+
+link.addEventListener("mouseleave", () => {
+  hoverTL.reverse();
+});
+// End
+
+// Start Animating Balls
+// End
+
+// modified animation to keep it centered on the viewport
+// modified animation timing
+// no background images
+// added stagger
+
+// simple js i ever seen :)
+
+const slider = document.querySelector(".slider");
+
+function activate(e) {
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener("click", activate, false);
